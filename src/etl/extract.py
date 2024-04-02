001f51905f13e8fc720a5f140890dc59e54318f5
alfonso <alfonso1003@users.noreply.github.com>
2024-04-01 22:52:40 -0600

    turn it into a class

--- src/etl/extract.py
+++ src/etl/extract.py
@@ -3,31 +3,49 @@ import urllib.error
 import urllib.request
 
 
-# https://www.census.gov/construction/bps/msamonthly.html
+class DataExtractor:
 
-today = date.today()
-current_year = today.year
-months = range(1,13)
-years = range(2004, current_year + 1)
-excel_file_start = date(2019, 11, 1)
+    today = date.today()
+    current_year = today.year
+    months = range(1, 13)
+    years = range(2004, current_year + 1)
+    excel_file_start = date(2019, 11, 1)
 
-for year in years:
-    for month in months:
+    def should_download(self, year, month):
+        return date(year, month, 1) <= self.today
 
-        report_year_month = date(year, month, 1)
-
-        if report_year_month > today:
-            continue
-
+    def build_url(self, year, month):
         url = f"https://www.census.gov/construction/bps/txt/tb3u{year}{month:02}.txt"
-        format = "txt"
-        if  report_year_month >= excel_file_start:
+        if date(year, month, 1) >= self.excel_file_start:
             url = f"https://www.census.gov/construction/bps/xls/msamonthly_{year}{month:02}.xls"
+        return url
+
+    def find_file_format(self, year, month):
+        format = "txt"
+        if date(year, month, 1) >= self.excel_file_start:
             format = "xls"
+        return format
 
-        output_file = f"./data/raw/bps_unit_{year}_{month:02}.{format}"
-
+    @staticmethod
+    def download_file(url, output_file):
         try:
             urllib.request.urlretrieve(url, output_file)
+            print(f"Downloaded: {output_file}")
         except urllib.error.URLError as e:
             print(f"{e}: {url}")
+
+    def download_data(self):
+        for year in self.years:
+            for month in self.months:
+                if not self.should_download(year, month):
+                    continue
+
+                url = self.build_url(year, month)
+                format = self.find_file_format(year, month)
+                output_file = f"./data/raw/bps_unit_{year}_{month:02}.{format}"
+                self.download_file(url, output_file)
+
+
+if __name__ == "__main__":
+    extractor = DataExtractor()
+    extractor.download_data()
